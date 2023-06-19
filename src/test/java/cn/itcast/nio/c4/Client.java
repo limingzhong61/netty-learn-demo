4e6cb29bc8bd1b74295f264d016e5c43e1e2f722
mzli <1162314270@qq.com>
2023-06-19 15:10:15 +0800

    init

---
+++ src/test/java/cn/itcast/nio/c4/Client.java
@@ -0,0 +1,19 @@
+package cn.itcast.nio.c4;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        SocketChannel sc = SocketChannel.open();
+        sc.connect(new InetSocketAddress("localhost", 8080));
+        SocketAddress address = sc.getLocalAddress();
+        //System.out.println("waiting...");
+        sc.write(Charset.defaultCharset().encode("0123456789abcdef3333\n"));
+        System.in.read();
+//        sc.close();
+    }
+}
